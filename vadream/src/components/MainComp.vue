b15eade6e04fff30f32453aac0be5c06925530a1
BbbGoat <oomi9421@gmail.com>
2024-02-04 22:35:01 +0900

    ss

--- vadream/src/components/MainComp.vue
+++ vadream/src/components/MainComp.vue
@@ -5,7 +5,9 @@
         <div class="top">
           <div class="menu">
             <div class="del-list">
-              <i>아이콘</i>
+              <i>
+                <font-awesome-icon :icon="['far', 'folder-open']" />
+              </i>
               <span>삭제 리스트</span>
             </div>
             <div class="upload-btn">
@@ -14,20 +16,86 @@
           </div>
         </div>
         <!-- 리스트1 -->
-        <section>
-          <h2>최근 재생 된 컨텐츠 (Carousel)</h2>
+        <section class="mid">
+          <h2 class="title">최근 재생 된 컨텐츠 (Carousel)</h2>
           <div class="slide">
             <!-- 스와이퍼 -->
+
+            <!-- 전체리스트 map -->
+            <div class="list">
+              <div class="inner">
+                <div class="img-area">
+                  <div class="icon" role="button" tabindex="1">
+                    <font-awesome-icon :icon="['fas', 'trash-can']" size="xl" />
+                  </div>
+                  <img src="/images/prd1.png" alt="상품 이미지">
+                </div>
+                <div class="info">
+                  <h3>영상제목</h3>
+                  <div class="line">
+                    <span>TYPE</span>
+                    <span>Video</span>
+                  </div>
+                  <div class="line">
+                    <span>SIZE</span>
+                    <span>20 MB</span>
+                  </div>
+                  <div class="line">
+                    <span>TIME</span>
+                    <span>01:00</span>
+                  </div>
+                </div>
+                <div class="btn-area">
+                  <!-- isAdd Boolean v-if 적용해서 토글버튼 -->
+                  <button class="add-btn" type="button">추가하기</button>
+                  <button class="added-btn" type="button">추가됨</button>
+                </div>
+              </div>
+            </div>
           </div>
         </section>
         <!-- 리스트2 -->
-        <section>
-          <h2>전체 컨텐츠</h2>
+        <section class="bot">
+          <h2 class="title">전체 컨텐츠</h2>
           <div class="contents">
             <!-- 전체리스트 map -->
             <div class="list">
               <div class="inner">
                 <div class="img-area">
+                  <div class="icon" role="button" tabindex="1">
+                    <font-awesome-icon :icon="['fas', 'trash-can']" size="xl" />
+                  </div>
+                  <img src="/images/prd1.png" alt="상품 이미지">
+                </div>
+                <div class="info">
+                  <h3>영상제목</h3>
+                  <div class="line">
+                    <span>TYPE</span>
+                    <span>Video</span>
+                  </div>
+                  <div class="line">
+                    <span>SIZE</span>
+                    <span>20 MB</span>
+                  </div>
+                  <div class="line">
+                    <span>TIME</span>
+                    <span>01:00</span>
+                  </div>
+                </div>
+                <div class="btn-area">
+                  <!-- isAdd Boolean v-if 적용해서 토글버튼 -->
+                  <button class="add-btn" type="button">추가하기</button>
+                  <button class="added-btn" type="button">추가됨</button>
+                </div>
+              </div>
+            </div>
+            
+            <div class="list">
+              <div class="inner">
+                <div class="img-area">
+                  <div class="icon" role="button" tabindex="1">
+                    <font-awesome-icon :icon="['fas', 'trash-can']" size="xl" />
+                  </div>
                   <img src="/images/prd1.png" alt="상품 이미지">
                 </div>
                 <div class="info">
@@ -55,7 +123,11 @@
           </div>
         </section>
       </article>
-      <aside class="right-side"></aside>
+      <aside class="right-side">
+        <div class="player">
+          플레이어
+        </div>
+      </aside>
     </div>
   </main>
 </template>
@@ -69,7 +141,7 @@ export default {
 
   },
   components: {
-    ButtonComp
+    ButtonComp,
   }  
 }
 </script>
@@ -80,12 +152,33 @@ export default {
   flex-direction: row;
 }
 
+/* /////////////// top /////////////// */
 .top {
   margin: 3rem;
 }
 .menu {
   display: flex;
   justify-content: end;
+  gap: 2rem;
+}
+.del-list {
+  font-size: 1.4rem;
+  background: #e5e5e5;
+  border-radius: 5px;
+  padding: 1.5rem;
+  cursor: pointer;
+}
+.del-list i {
+  margin-right: 0.4rem;
+}
+
+/* /////////////// section /////////////// */
+.title {
+  font-size: 2rem;
+  margin-bottom: 2rem;
+}
+.contents {
+  display: grid;
 }
 .list {
   width: 170px;
@@ -93,9 +186,9 @@ export default {
   border-radius: 7px;
   overflow: hidden;
 }
-
 /* 이미지영역 */
 .img-area {
+  position: relative;
   height: 120px;
   background-color: #ddd;
 }
@@ -103,6 +196,12 @@ export default {
   width: 100%;
   height: 100%;
   object-fit: cover;
+}
+.icon {
+  position: absolute;
+  right: 0;
+  margin: 0.4rem;
+  cursor: pointer;
 }
 
 /* 정보영역 */
@@ -154,13 +253,23 @@ export default {
 }
 
 
+
 aside.right-side {
   width: 30%;
-  height: 100vh;
-  background: burlywood;
+  background: oldlace;
+}
+.player {
+  position: sticky;
+  top: 3rem;
+  height: 600px;
+  background: #fff;
+  margin: 3rem;
 }
 article.left-side {
   width: 70%;
-  /* height: 100vh; */
+  height: 1200px;
+}
+article > section {
+  padding: 3rem;
 }
 </style>
